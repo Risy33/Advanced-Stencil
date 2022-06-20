46ff08768d5aa00ad8eb86464c2c7875b45a2f1f
Risy33 <dario.risitano@gmail.com>
2022-06-20 17:24:42 +0200

    two way binding and input validation

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -9,15 +9,29 @@ import { AV_API_KEY } from '../../global/global';
 })
 export class StockPrice {
   @State() fetchedPrice: number;
+  @State() stockUserinput: string;
+  @State() stockInputValid = false;
 
-  @Element() el: HTMLElement;
+  onUserInput(event: Event) {
+    this.stockUserinput = (event.target as HTMLInputElement).value;
+    if (this.stockUserinput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
+  }
+
+  stonckInput: HTMLInputElement;
+
+  //   @Element() el: HTMLElement;
 
   onFetchStockPrice(event: Event) {
     event.preventDefault();
 
-    const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stonckInput.value; //this alternative will it's used when ref is used
 
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}=${AV_API_KEY}`)
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
       .then(res => {
         return res.json();
       })
@@ -30,8 +44,10 @@ export class StockPrice {
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input type="text" id="stock-symbol" />
-        <button type="submit">Fetch</button>
+        <input type="text" id="stock-symbol" ref={el => (this.stonckInput = el)} value={this.stockUserinput} onInput={this.onUserInput.bind(this)} />
+        <button type="submit" disabled={!this.stockInputValid}>
+          Fetch
+        </button>
       </form>,
       <div>
         <p>Price: $ {this.fetchedPrice}</p>
